ec262f46a316f306f5a5b873214457b0f380b2b2
Jamie Tarquini <pmpknface@gmail.com>
2026-06-05 00:17:30 -0400

    Update dataframe display to use Pandas
    Comment out the original dataframe display and convert the Snowpark dataframe to a Pandas dataframe for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,12 @@ st.write('The name on your smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col(SEARCH_ON))
-st.dataframe(data=my_dataframe, use_container_width=true)
+#st.dataframe(data=my_dataframe, use_container_width=true)
+#st.stop()
+
+#Convert teh snowpark df to a pandas df so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
