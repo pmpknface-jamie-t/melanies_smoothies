b1fd80a2c3a8919c2d95c40b649a62e6c44ccbe6
Jamie Tarquini <pmpknface@gmail.com>
2026-06-04 23:39:47 -0400

    Display smoothie data in a dataframe instead of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,8 @@ if ingredients_list:
     
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json())
+    # st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
