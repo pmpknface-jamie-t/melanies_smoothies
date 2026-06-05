b50111ceffa473ebdcd72e85073c24a3fd6dcf43
Jamie Tarquini <pmpknface@gmail.com>
2026-06-04 23:36:57 -0400

    Fix response handling for Smoothiefroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ if ingredients_list:
     
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
