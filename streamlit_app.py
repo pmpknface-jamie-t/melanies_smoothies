41112a849286d2f407cc4bda1e1839cf0eef876f
Jamie Tarquini <pmpknface@gmail.com>
2026-06-04 23:35:05 -0400

    Refactor smoothie order submission and API call
    Removed table, info added

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ st.write('The name on your smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -23,24 +22,17 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    st.write(ingredients_string)
+    
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)  
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
 time_to_insert = st.button('Submit Order')
-
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-
-  
