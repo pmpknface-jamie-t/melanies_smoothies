bba50fefe430e5f2c0003b3399d674a6440fc186
Jamie Tarquini <pmpknface@gmail.com>
2026-06-05 00:22:04 -0400

    Comment out dataframe display and use LOC
    Comment out dataframe display and stop calls for debugging.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,10 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=true)
 #st.stop()
 
-#Convert teh snowpark df to a pandas df so we can use the LOC function
+#Convert the snowpark df to a pandas df so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -31,8 +31,13 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
